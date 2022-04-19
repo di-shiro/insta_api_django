e65cb271f356db6e4eec57a94dbed8ba7cf1baa4
DI-AQUARIUS <aquarius@aquarius-air.local>
2022-04-19 17:38:31 +0900

    Admin dashbord 編集完了

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from . import models
 
-# Register your models here.
+
+
+
+
+'''
+今回は、models.pyの中で、UserModelをOverrideしてカスタマイズしてしまったので、
+このAdminで表示されるレイアウト等もOveerrideして変更を加える必要がある。
+以下のクラスは、DjangoのGUIレイアウトに対応している。
+'''
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ()}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password_1', 'password_2'),
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Profile)
+admin.site.register(models.Post)
+admin.site.register(models.Comment)
+
+
+
+
